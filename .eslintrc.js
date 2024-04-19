0716045a47eef2707f20c1e4c46e22d5c8467cad
DanishAli097 <danish@codendevs.com>
2024-04-20 02:08:05 +0500

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,12 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        "eqeqeq": "error", // enforce the use of === and !==
+        "curly": "error", // require curly braces for all control statements
+        "no-console": "warn", // disallow the use of console
+        "no-debugger": "warn", // disallow the use of debugger
+        "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }], // disallow unused variables except those starting with an underscore
+        "@typescript-eslint/explicit-function-return-type": "off", // do not require explicit return types on functions
+        "@typescript-eslint/no-explicit-any": "off" // do not disallow usage of the any type
     }
 }
